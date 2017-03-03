3ddaede356843ec509d4f534cd51cc145bcad428
jonareyes <jonathan_reyesv@hotmail.com>
2017-03-03 00:19:51 -0600

    This exersice is just to analyze the problem

---
+++ analizar_variables.rb
@@ -0,0 +1,19 @@
+#In this case we treat to obtain the result about this exercise, whitout doing anything of code.
+
+suma_uno = 4 + 7
+suma_dos = suma_uno + 8
+suma_dos == 19
+true
+
+resta_uno = 3 - 2
+valor = "uno"
+resta_dos = resta_uno - valor
+resta_dos == 0
+true
+
+multiplicacion = 6 * 5
+division = multiplicacion / 5
+operacion = division - 3
+cadena = "Hola" * operacion
+cadena == "HolaHolaHolaHola"
+false
